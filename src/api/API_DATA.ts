7e5011ceae437a46b0484201ac75b6a62c27afae
Danyil Liubchak <danik.liubchak@gmail.com>
2024-10-11 13:39:36 -0400

    0.0.1 deploy project

--- src/api/API_DATA.ts
+++ src/api/API_DATA.ts
@@ -1,5 +1,5 @@
 export const ClientID = "b00dc8c136614d7f909617415763c3c4";
-export const RedirectURI = "http://localhost:3000/";
+export const RedirectURI = window.location.origin + "/";
 export const apiURL = "https://accounts.spotify.com/authorize";
 // scope can do not use for more data
 export const scope = [
